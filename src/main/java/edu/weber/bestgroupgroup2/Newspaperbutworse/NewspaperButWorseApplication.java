f8700b6bb623778afce1e631e54b87afd257159c
Jazlyn McKenzie Maxwell <jazlyn.maxwell@gmail.com>
2021-10-05 20:04:20 -0600

    created spring project

---
+++ src/main/java/edu/weber/bestgroupgroup2/Newspaperbutworse/NewspaperButWorseApplication.java
@@ -0,0 +1,13 @@
+package edu.weber.bestgroupgroup2.Newspaperbutworse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaperButWorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspaperButWorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/NewspaperButWorseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.weber.bestgroupgroup2.Newspaperbutworse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaperButWorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
