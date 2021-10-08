6714fa8262ab2a30758128c9a088057a7e2795a9
countder3k <der3kburrola@gmail.com>
2021-10-07 19:09:01 -0600

    Added navbar to index

--- src/main/java/edu/weber/bestgroupgroup2/Newspaperbutworse/HomeController.java
+++ src/main/java/edu/weber/bestgroupgroup2/Newspaperbutworse/HomeController.java
@@ -1,5 +1,16 @@
 package edu.weber.bestgroupgroup2.Newspaperbutworse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HomeController {
+	@GetMapping("/")
+	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
+		model.addAttribute("name", name);
+		return "/index";
+	}
 
 }
