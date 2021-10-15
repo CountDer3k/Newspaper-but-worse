1f24107fb4f3c03a5af0814df53a2bf5e7b73739
GlitchJack <52464232+GlitchJack@users.noreply.github.com>
2021-10-14 20:54:30 -0600

    MINE,MYWORKALLJACKS

--- src/main/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserService.java
+++ src/main/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserService.java
@@ -1,13 +1,20 @@
 package edu.weber.bestgroupgroup2.Newspaperbutworse.User;
 
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+	private static UserService INSTANCE;
 	//Should be Autowired
     private UserRepository userRepository;
+    
+    public UserService() {
+    	
+    }
     
     public User registerNewUserAccount(User user) {
         return user;
