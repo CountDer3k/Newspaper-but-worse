534d9e6b1c67ad7aabaeb2d81aed88eb2c3276ee
Der3k <42587743+CountDer3k@users.noreply.github.com>
2021-11-02 21:03:54 -0600

    Added JUnit 4 dependency

--- src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/Post/PostServiceTest.java
+++ src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/Post/PostServiceTest.java
@@ -1,6 +1,6 @@
 package edu.weber.bestgroupgroup2.Newspaperbutworse.Post;
 
-import org.junit.Test;
+
 
 import static org.mockito.Mockito.when;
 
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
