227df94134d675f72a8b916aed2205033156a61b
Jazlyn McKenzie Maxwell <jazlyn.maxwell@gmail.com>
2021-11-04 23:08:09 -0600

    fixed errors created during merger

--- src/main/java/edu/weber/bestgroupgroup2/Newspaperbutworse/HomeController.java
+++ src/main/java/edu/weber/bestgroupgroup2/Newspaperbutworse/HomeController.java
@@ -35,27 +35,28 @@ public class HomeController {
 	@GetMapping("/")
 	@Log
 	public ModelAndView home(){
-		ModelAndView modelAndView = new ModelAndView("index");
-		List<PostModel> postList = new ArrayList<PostModel>();
-
-		ArticleModel article1 = new ArticleModel("Slytherin Quidditch Through The Ages","This is the first article");
-		ArticleModel article2 = new ArticleModel("Hogwarts, A History","Hogwarts: A History, also known as Hogwarts, A History, is a book concerning Hogwarts School of Witchcraft and Wizardry and its history that was written by Bathilda Bagshot[1]. It was Hermione Granger's favourite book and she often referred to this book on many things concerning Hogwarts School of Witchcraft and Wizardry. Three of the things that are frequently brought up are the Great Hall's enchanted ceiling that shows the weather outside, the fact that you cannot apparate or disapparate on Hogwarts grounds and the fact that electronic devices do not work within the grounds.\n"
-				+ "\n"
-				+ "Incredible PC game bundle, from $10\n"
-				+ "\n"
-				+ "BUY FROM FANATICAL\n"
-				+ "The book was also very popular in Harry Potter's second year at Hogwarts, when every copy was checked out of the library due to the reopening of the Chamber of Secrets. However, Hermione shows slight frustration with the book when scathingly renaming the book in her fourth year because it does not mention the use of house-elves at Hogwarts, even going so far as to suggest a couple of alternative titles for it: A Revised History of Hogwarts and A Highly Biased and Selective History of Hogwarts Which Glosses Over the Nastier Aspects of the School.");
-
-		PostModel pm1 = new PostModel(3, 10, article1);
-		PostModel pm2 = new PostModel(7, 11, article2);
-
-		postList.add(pm1);
-		postList.add(pm2);
-
-		modelAndView.getModelMap().addAttribute("posts", postList);
-		modelAndView.getModelMap().addAttribute("name", name);
 		
-		return modelAndView;
+		ModelAndView modelAndView = new ModelAndView("index");
+		
+		try {
+			// Get all posts from the db
+			List<PostArticleModel> posts = new ArrayList<PostArticleModel>();
+			posts = postService.getAllPosts();
+			
+			// If no posts are available show the empty content div
+			if(posts.size() == 0) {
+				modelAndView.getModelMap().addAttribute("isEmpty", true);
+				modelAndView.getModelMap().addAttribute("empty", "No content available to display...");
+			}else {
+				modelAndView.getModelMap().addAttribute("isEmpty", false);
+				modelAndView.getModelMap().addAttribute("posts", posts);
+				modelAndView.getModelMap().addAttribute("name", name);
+			}
+			return modelAndView;
+			} catch(Exception e) {
+				logger.error("" + e.toString());
+				return null;
+			}
 	}
 	
 }
