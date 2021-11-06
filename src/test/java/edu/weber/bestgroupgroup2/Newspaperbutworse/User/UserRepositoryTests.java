fe0b263ad0011a6c6765acfa69c459eb67bb5570
Jazlyn McKenzie Maxwell <jazlyn.maxwell@gmail.com>
2021-11-06 12:29:33 -0600

    unit test now fails instead of producing errors. progrss.

--- src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserRepositoryTests.java
+++ src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserRepositoryTests.java
@@ -5,26 +5,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.assertj.core.util.Arrays;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTests {
 	
-	@Autowired
 	UserRepository userRepository;
-	
-	@Mock
+
+	@Autowired
 	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	private List<User> users = new ArrayList<User>(List.of(
+			new User("spiderman", "pparker@dailybugle.net", "Peter", "Parker"),
+			new User("jjjjr", "jjjameson@dailybugle.net", "J Jonah", "Jameson")));
 	
 	@Before
 	public void setup() {
@@ -35,22 +40,16 @@ public class UserRepositoryTests {
 	@Test
 	public void testGetUserByUsername() {
 		String username = "spiderman";
-		System.out.println(username);
-		List<User> expectedList = entities()
-				.map(userRepository::save)
+		users.stream().map(userRepository::save);
+		List<User> expectedList = users.stream()
 				.filter(user -> user.getUsername().equals(username))
 				.collect(Collectors.toList());
 		List<User> actualList = new ArrayList<>();
 		actualList.add(userRepository.getUserByUsername(username));
-		
+		users.stream().forEach(user -> System.out.println(user));
+		System.out.println(expectedList);
+		System.out.println(actualList);
 		Assert.assertEquals(expectedList, actualList);
-	}
-	
-	private Stream<User> entities() {
-		return Stream.of(
-				new User("spiderman", "pparker@dailybugle.net", "Peter", "Parker"),
-				new User("jjjjr", "jjjameson@dailybugle.net", "J Jonah", "Jameson")
-			);
 	}
 	
 }
