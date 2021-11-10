680bf381d03b92b56bddb7a74b6f706b74fe5690
Jazlyn McKenzie Maxwell <jazlyn.maxwell@gmail.com>
2021-11-09 20:03:23 -0700

    configured to use the local database

--- src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserRepositoryTests.java
+++ src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserRepositoryTests.java
@@ -1,29 +1,23 @@
 package edu.weber.bestgroupgroup2.Newspaperbutworse.User;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @DataJdbcTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTests {
 	
-	@Autowired
 	UserRepository userRepository;
-	
-	@Mock
 	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
 	@Before
@@ -36,14 +30,13 @@ public class UserRepositoryTests {
 	public void testGetUserByUsername() {
 		String username = "spiderman";
 		System.out.println(username);
-		List<User> expectedList = entities()
+		User expected = entities()
 				.map(userRepository::save)
-				.filter(user -> user.getUsername().equals(username))
-				.collect(Collectors.toList());
-		List<User> actualList = new ArrayList<>();
-		actualList.add(userRepository.getUserByUsername(username));
+				.filter(user -> user.getUsername().equals(username)).
+				findFirst().get();
+		User actual = (userRepository.getUserByUsername(username));
 		
-		Assert.assertEquals(expectedList, actualList);
+		Assert.assertEquals(expected, actual);
 	}
 	
 	private Stream<User> entities() {
