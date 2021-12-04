b0142f205f6f6e3d09540a5dcfc7fcb88168d4e4
Der3k <42587743+CountDer3k@users.noreply.github.com>
2021-12-04 14:01:03 -0700

    45% Test coverage

--- src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/Post/PostControllerTest.java
+++ src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/Post/PostControllerTest.java
@@ -1,6 +1,7 @@
 package edu.weber.bestgroupgroup2.Newspaperbutworse.Post;
 
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.sql.Date;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.ModelAndView;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PostControllerTest {
@@ -38,6 +40,92 @@ public class PostControllerTest {
 	
 	@Test
 	public void testShowArticleView() {
+		ModelAndView expected = new ModelAndView("article/article");
+		
+		PostArticleModel pam = makePAM();
+		List<Comment> comments = new ArrayList<Comment>();
+		comments.add(new Comment());
+		
+		when(postService.getPostWithAuthorByID(any(String.class))).thenReturn(pam);
+		when(postService.getCommentsFromArticle(any(int.class))).thenReturn(null);
+		
+		ModelAndView actual = postController.showArticleView("1");
 
+		Assert.assertEquals(expected.getViewName(), actual.getViewName());
+	}
+	
+	@Test
+	public void testShowRegistrationForm() {
+		ModelAndView expected = new ModelAndView("article/articleForm");
+
+		ModelAndView actual = postController.showRegistrationForm(null);
+
+		Assert.assertEquals(expected.getViewName(), actual.getViewName());
+	}
+	
+	@Test
+	public void testEditableArticleDirect() {
+		ModelAndView expected = new ModelAndView("article/article_edit");
+
+		ModelAndView actual = postController.showEditableArticleDirect(any(String.class));
+
+		Assert.assertEquals(expected.getViewName(), actual.getViewName());
+	}
+	
+	@Test
+	public void testShowAuthorArticle() {
+		ModelAndView expected = new ModelAndView("article/article_edit");
+
+		ModelAndView actual = postController.showEditableArticleDirect(any(String.class));
+
+		Assert.assertEquals(expected.getViewName(), actual.getViewName());
+	}
+	
+	
+	
+	//------------------------
+	// Helper creation methods
+	//------------------------
+
+	public PostArticleModel makePAM() {
+		return makePAM(makePost());
+	}
+
+	public PostArticleModel makePAM(PostModel post) {
+		PostArticleModel pam = new PostArticleModel();
+		pam.setPost(post);
+		pam.setName("Dobby Elf");
+		return pam;
+	}
+	
+	public PostModel makePost() {
+		PostModel post = new PostModel();
+		ArticleModel article = makeArticle();
+
+		post.setUserId(1);
+		long millis = System.currentTimeMillis();  
+		post.setCreateDate(new Date(millis));
+		post.setArticle(article);
+
+		return post;
+	}
+
+	public PostDto makePostDto() {
+		PostDto article = new PostDto();
+
+		article.setTitle("another testing article");
+		article.setContent("This is yet another article with some nice content... yum ;)");
+		article.setAccess("FR");
+
+		return article;
+	}
+
+	public ArticleModel makeArticle() {
+		ArticleModel article = new ArticleModel();
+		article.setTitle("another testing article");
+		article.setContent("This is yet another article with some nice content... yum ;)");
+		article.setAccess("FR");
+
+		return article;
 	}
 }
