6f78f3a6c1d2faa6a80950b9e826deea5ab252d5
Jazlyn McKenzie Maxwell <jazlyn.maxwell@gmail.com>
2021-12-06 21:45:02 -0700

    added testIsValidUserTrue()

--- src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserServiceTests.java
+++ src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/User/UserServiceTests.java
@@ -1,6 +1,10 @@
 package edu.weber.bestgroupgroup2.Newspaperbutworse.User;
 
+import java.util.ArrayList;
+
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,5 +25,35 @@ public class UserServiceTests {
 		userService = new UserService(userRepository, passwordEncoder);
 	}
 	
+	@Test
+	@Log
+	public void testIsValidUserTrue() {
+		UserDto user = getGwenStacy();
+		Assert.assertTrue(userService.isValidUser(user));
+	}
+	
+	@Test
+	@Log
+	public void testIsValidUserFalse_Username() {
+		
+	}
+	
+	/* Helper Methods */
+	public UserDto getGwenStacy() {
+		UserDto user = new UserDto();
+		user.setUsername("GhostSpider");
+		user.setPassword("123456789");
+		user.setMatchingPassword("123456789");
+		user.setEmail("gwen.stacy@gmail.com");
+		user.setFirstName("Gwendolyne");
+		user.setLastName("Stacy");
+		
+		ArrayList<Role> roles = new ArrayList<Role>();
+		Role role = new Role();
+		role.setRoleId(4);
+		roles.add(role);
+		user.setRoles(roles);
+		return user;
+	}
 	
 }
