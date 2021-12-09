14ae4c0b9d9c49d5c710d2de0087d5edb2bfb3ba
Der3k <42587743+CountDer3k@users.noreply.github.com>
2021-12-08 23:51:30 -0700

    62.7%

--- src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/REST/RESTTest.java
+++ src/test/java/edu/weber/bestgroupgroup2/Newspaperbutworse/REST/RESTTest.java
@@ -47,145 +47,200 @@ public class RESTTest {
 	UserRepository userRepo;
 	@Mock
 	PasswordEncoder encoder;
-	
+
 	@Before
 	public void setup() {
 		controller = new RESTPostController(service);
 	}
-	
+
 	@Test
 	public void testCreateArticle() {
 		PostModel pm = makePost();
 		pm.setId(1);
 		ResponseEntity<Object> expected = ResponseEntity.ok(pm);
 		PostDto dto = new PostDto();
-		
+
 		when(service.addNewPost(any(PostDto.class), anyInt())).thenReturn(pm);
-		
+
 		ResponseEntity<Object> actual = controller.createArticle("1", any(String.class), any(String.class));
-		
+
 		Assert.assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testCreateArticle_null() {
 		PostModel pm = makePost();
 		pm.setId(1);
 		ResponseEntity<Object> expected = ResponseEntity.ok("Article could not be added");
 		PostDto dto = new PostDto();
-		
+
 		when(service.addNewPost(dto, 3)).thenReturn(pm);
-		
+
 		ResponseEntity<Object> actual = controller.createArticle("1", any(String.class), any(String.class));
-		
+
 		Assert.assertEquals(expected, actual);
 	}
-	
+
+	@Test
+	public void testCreateArticle_exception() {
+		PostModel pm = makePost();
+		pm.setId(1);
+		ResponseEntity<Object> expected = ResponseEntity.ok("Internal error occured");
+		PostDto dto = new PostDto();
+
+		when(service.addNewPost(any(PostDto.class), anyInt())).thenReturn(pm);
+
+		ResponseEntity<Object> actual = controller.createArticle("abc", any(String.class), any(String.class));
+
+		Assert.assertEquals(expected, actual);
+	}
+
 	@Test
 	public void testGetAllPost(){
 		List<PostArticleModel> posts = new ArrayList<PostArticleModel>();
 		ResponseEntity<List<PostArticleModel>> expected = ResponseEntity.ok(posts);
-		
+
 		when(service.getAllPosts(anyInt(), anyInt())).thenReturn(posts);
-		
+
 		ResponseEntity<List<PostArticleModel>> actual = controller.getAllPost("2", "1");
 		Assert.assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testGetAllPost_pageNum_Null(){
 		List<PostArticleModel> posts = new ArrayList<PostArticleModel>();
 		ResponseEntity<List<PostArticleModel>> expected = ResponseEntity.ok(posts);
-		
+
 		when(service.getAllPosts(anyInt(), anyInt())).thenReturn(posts);
-		
+
 		ResponseEntity<List<PostArticleModel>> actual = controller.getAllPost("abc", null);
 		Assert.assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testGetAllPostForAuthor() {
 		List<PostArticleModel> posts = new ArrayList<PostArticleModel>();
 		ResponseEntity<List<PostArticleModel>> expected = ResponseEntity.ok(posts);
-		
+
 		when(service.getAllPostsForUserWithId("2")).thenReturn(posts);
-		
+
 		ResponseEntity<List<PostArticleModel>> actual = controller.getAllPostForAuthor("2", "2" , "2");
 		Assert.assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testGetAllPostForAuthor_Null() {
 		List<PostArticleModel> posts = new ArrayList<PostArticleModel>();
 		ResponseEntity<List<PostArticleModel>> expected = ResponseEntity.ok(posts);
-		
+
 		when(service.getAllPostsForUserWithId("2")).thenReturn(posts);
-		
+
 		ResponseEntity<List<PostArticleModel>> actual = controller.getAllPostForAuthor("2", "2" , null);
 		Assert.assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testGetAllPostForAuthor_Exception() {
 		List<PostArticleModel> posts = new ArrayList<PostArticleModel>();
 		ResponseEntity<List<PostArticleModel>> expected = ResponseEntity.ok(posts);
-		
+
 		when(service.getAllPostsForUserWithId("2")).thenReturn(posts);
-		
+
 		ResponseEntity<List<PostArticleModel>> actual = controller.getAllPostForAuthor("2", "abc" , null);
 		Assert.assertEquals(expected, actual);
 	}
+
+	@Test
+	public void testGetPost() {
+		PostModel p = makePost();
+		ResponseEntity<PostModel> expected = ResponseEntity.ok(p);
+
+		when(service.getPostByID("1")).thenReturn(p);
+
+		ResponseEntity<Object> actual = controller.getPost("1");
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testDeletePost_true() {
+		ResponseEntity<Boolean> expected = ResponseEntity.ok(true);
+
+		when(service.deletePost("1")).thenReturn(true);
+
+		ResponseEntity<Boolean> actual = controller.deletePost("1");
+		Assert.assertEquals(expected, actual);
+	}
 	
-	
+	@Test
+	public void testDeletePost_false() {
+		ResponseEntity<Boolean> expected = ResponseEntity.ok(false);
+
+		when(service.deletePost("1")).thenReturn(false);
+
+		ResponseEntity<Boolean> actual = controller.deletePost("1");
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testGetPost_null() {
+		PostModel p = new PostModel();
+		ResponseEntity<PostModel> expected = ResponseEntity.ok(p);
+
+		when(service.getPostByID(any(String.class))).thenReturn(p);
+
+		ResponseEntity<Object> actual = controller.getPost(any(String.class));
+		Assert.assertEquals(expected, actual);
+	}
+
 	//------------------------
-		// Helper creation methods
-		//------------------------
+	// Helper creation methods
+	//------------------------
 
-		public PostArticleModel makePAM() {
-			return makePAM(makePost());
-		}
-		
-		public PostArticleModel makePAM(PostModel post) {
-			PostArticleModel pam = new PostArticleModel();
-			pam.setPost(post);
-			pam.setName("Dobby Elf");
-			return pam;
-		}
-		
-		public void createNewUser() {
-			UserDto user = new UserDto();
-			
-			user.setUsername("Dobby The House Elf");
-			user.setFirstName("Dobby");
-			user.setLastName("Elf");
-			user.setEmail("dobby_with_hats@hogwarts.com");
-			user.setPassword("harry-potter");
-			user.setMatchingPassword("harry-potter");
-			
-			UserService us = new UserService(userRepo, encoder);
-			us.registerNewUserAccount(user);
-			
-		} 
+	public PostArticleModel makePAM() {
+		return makePAM(makePost());
+	}
 
-		
-		public PostModel makePost() {
-			PostModel post = new PostModel();
-			ArticleModel article = makeArticle();
+	public PostArticleModel makePAM(PostModel post) {
+		PostArticleModel pam = new PostArticleModel();
+		pam.setPost(post);
+		pam.setName("Dobby Elf");
+		return pam;
+	}
 
-			post.setUserId(1);
-			long millis = System.currentTimeMillis();  
-			post.setCreateDate(new Date(millis));
-			post.setArticle(article);
-			
-			return post;
-		}
+	public void createNewUser() {
+		UserDto user = new UserDto();
 
-		public ArticleModel makeArticle() {
-			ArticleModel article = new ArticleModel();
-			article.setTitle("another testing article");
-			article.setContent("This is yet another article with some nice content... yum ;)");
-			article.setAccess("FR");
-			
-			return article;
-		}
+		user.setUsername("Dobby The House Elf");
+		user.setFirstName("Dobby");
+		user.setLastName("Elf");
+		user.setEmail("dobby_with_hats@hogwarts.com");
+		user.setPassword("harry-potter");
+		user.setMatchingPassword("harry-potter");
+
+		UserService us = new UserService(userRepo, encoder);
+		us.registerNewUserAccount(user);
+
+	} 
+
+
+	public PostModel makePost() {
+		PostModel post = new PostModel();
+		ArticleModel article = makeArticle();
+
+		post.setUserId(1);
+		long millis = System.currentTimeMillis();  
+		post.setCreateDate(new Date(millis));
+		post.setArticle(article);
+
+		return post;
+	}
+
+	public ArticleModel makeArticle() {
+		ArticleModel article = new ArticleModel();
+		article.setTitle("another testing article");
+		article.setContent("This is yet another article with some nice content... yum ;)");
+		article.setAccess("FR");
+
+		return article;
+	}
 }
